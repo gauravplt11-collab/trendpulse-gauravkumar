6d70fb154cb19ee9ee8e1b77f2cf941a468f6e7f
gauravplt11-collab <gauravplt11@gmail.com>
2026-04-01 22:50:09 +0530

    Update task4_visualization.py

--- task4_visualization.py
+++ task4_visualization.py
@@ -1,1 +1,19 @@
+# Task 4: Visualization
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Load processed data
+df = pd.read_csv('processed_data.csv')
+
+# Plot trend lengths
+plt.figure()
+df['length'].plot(kind='bar')
+
+plt.title("Length of Trending Topics")
+plt.xlabel("Index")
+plt.ylabel("Length")
+
+# Save plot
+plt.savefig("trend_visualization.png")
+
+print("Visualization saved as trend_visualization.png")
